3ca3de482e926749ba49dc25a58ace49b5d0452a
Rocio Carmona <rociodelcarmen29022000@gmail.com>
2023-06-13 19:37:40 +0100

    update

---
+++ 102/lab-07/app.js
@@ -0,0 +1,19 @@
+function capital(p1){
+    loop:
+    while (true){
+        if (p1 == "madrid"){
+            alert("Correct!")
+            break
+        } else if (p1 == "barcelona"){
+            alert("Your answer is incorrect")
+            break
+        } else{
+            alert("This is not a valid answer")
+            p1 = prompt("What is the capital of Spain? Please choose between Madrid or Barcelona")
+        }
+
+    }
+    
+}
+ 
+p1 = prompt("What is the capital of Spain? Please choose between Madrid or Barcelona")
