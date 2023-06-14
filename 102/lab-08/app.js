6dcf084dedd8d773ddd274e2b71a9712d575b9d9
Rocio Carmona <rociodelcarmen29022000@gmail.com>
2023-06-14 15:02:40 +0100

    update

--- 102/lab-08/app.js
+++ 102/lab-08/app.js
@@ -18,32 +18,23 @@ function capital(p1){
  
 p1 = prompt("What is the capital of Spain? Please choose between Madrid or Barcelona")
 
-// let output
-// let rating
-// function imageDisplay(){ 
-//     rating = prompt("How many times do you want me to display an image for? please choose between 1-5")
-//     while(rating = true){
-//         if (rating == 1 || rating == 2 || rating == 3  || rating == 4 || rating == 5 ){
-//             for (let i = 0; i < rating; i++) 
-//             output += "<img src='madrid-CC-pic.jpg' alt='Image of madrid CC' class='img--class'/>"
+
+function imageDisplay(){ 
+    let output = ""
+    let rating = prompt("How many times do you want me to display an image for? please choose between 1-5")
+    while(rating){
+        if (rating == 1 || rating == 2 || rating == 3  || rating == 4 || rating == 5 ){
+            for (let i = 0; i < rating; i++) 
+            output += "<img src='madrid-CC-pic.jpg' alt='Image of madrid CC' class='img--class'/>"
             
-//             break
-//         } else{
-//             alert("This is not a valid answer")
-//             rating = prompt("How many times do you want me to display an image for? please choose between 1-5")
-//         }
-         
-//     }
-//     document.write(output)
-// }
-
-
- function displayRating() {
-    let output = "";
-    let rating = prompt("On a scale of 1 - 5, how much do you rate chris's haircut?");
-    for (let i = 0; i < rating; i++) {
-      output += "<img class='star-img' src='madrid-CC-pic.jpg' alt='pic of madrid CC'>";
+            break
+        } else{
+            alert("This is not a valid answer")
+            rating = prompt("How many times do you want me to display an image for? please choose between 1-5")
+        }
+        
     }
-    return document.write(output);
-  }
+    return document.write(output)
+}
+
 
