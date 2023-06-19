b0c62cfae5b95a7be6768116f61fd136c66c198b
Rocio Carmona <rociodelcarmen29022000@gmail.com>
2023-06-19 23:48:43 +0100

    started lab 01

--- 201/lab-01/app.js
+++ 201/lab-01/app.js
@@ -1,3 +1,10 @@
 "use strict"
 
 console.log("Hi")
+
+let q1 = prompt("What is your name?")
+let q2 = prompt("What is todays date?")
+let q3 = prompt("How many siblings do you have?")
+let q4 = prompt("What is your favourite colour?")
+
+alert(`Hi ${q1} , I see, so today is ${q2} ?. You have ${q3} siblings and your favourite colour is ${q4} is that right? `)
